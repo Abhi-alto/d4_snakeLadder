fc46052b3f01fffd6cfde5c61ab40606e339a5dd
unknown <abhisheksrivastava9719@gmail.com>
2022-06-15 12:29:41 +0530

    Added UC4 file

--- Program.cs
+++ Program.cs
@@ -5,24 +5,31 @@ const int Start = 0, End = 100;                          //represent start posit
 int position = Start;                                      //current position of the player
 Console.WriteLine(n+" is at the " + position + " position in "+" the starting");
 
-int dice = new Random().Next(1, 7);                         //gets value dice value
-Console.WriteLine(n + " has got " + dice);
-position=position+dice;
-Console.WriteLine("New position of "+n+" is " + position); //initially let the player throw the dice and then check whether he has got a ladder or a snake or nothing
+while (position <= 100)                                 
+{
+    int dice = new Random().Next(1, 7);                         //gets value dice value
+    Console.WriteLine(n + " has got " + dice);
 
-int opt=new Random().Next(0, 3);                            //three values
-if(opt==0)
-{
-    Console.Write("No Play .... remain at");
+    Console.Write("Lets see ");
+    int opt = new Random().Next(0, 3);                            //three values
+    if (opt == 0)
+    {
+        Console.Write("No Play .... remain at ");
+    }
+    else if (opt == 1)
+    {
+        Console.Write("Earned a Ladder ......jumped to ");
+        position = position + dice;
+    }
+    else
+    {
+        Console.Write("Snake .... back to ");
+        position = position - dice;
+        if (position < 0)                               //Restart condition
+        {
+            position = 0;
+        }
+    }
+    Console.WriteLine(position);                        //prints the position at the end of one dice throwing process
 }
-else if(opt==1)
-{
-    Console.Write("Earned a Ladder ......jumped to  ");
-    position = position + dice;
-}
-else
-{
-    Console.Write("Snake .... back to");
-    position = position - dice;
-}
-Console.WriteLine("position " + position); 
+Console.WriteLine("Won the game");
