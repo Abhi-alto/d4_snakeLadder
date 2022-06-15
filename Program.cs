46a760e03e65d9df60375a6ceaac8784d4408a8d
unknown <abhisheksrivastava9719@gmail.com>
2022-06-15 11:18:21 +0530

    Added UC1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to snake and Ladder game");
+Console.WriteLine("Enter player name");
+String n = Console.ReadLine();
+const int Start = 0, End = 100;
+int position = Start;
+Console.WriteLine(n+" is at the " + position + " position");
