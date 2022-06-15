933e2a0d2749f28f1b77715f9e9f489dff88562e
unknown <abhisheksrivastava9719@gmail.com>
2022-06-15 11:35:32 +0530

    Added Uc2 file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-Console.WriteLine("Welcome to the Snake and Ladder game");
-﻿Console.WriteLine("Welcome to snake and Ladder game");
+Console.WriteLine("Welcome to snake and Ladder game");
 Console.WriteLine("Enter player name");
 String n = Console.ReadLine();
 const int Start = 0, End = 100;
 int position = Start;
-Console.WriteLine(n+" is at the " + position + " position");
+Console.WriteLine(n+" is at the " + position + " position"+" the starting");
+
+int dice = new Random().Next(1, 7);
+Console.WriteLine(n+" has got "+dice);
