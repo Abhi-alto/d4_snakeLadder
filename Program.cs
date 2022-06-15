8778cda068e5b922cade2dd727a9694e6745ed84
unknown <abhisheksrivastava9719@gmail.com>
2022-06-15 12:56:29 +0530

    Added Uc6 file

--- Program.cs
+++ Program.cs
@@ -2,13 +2,15 @@ Console.WriteLine("Welcome to snake and Ladder game");
 Console.WriteLine("Enter player name");                     //for single player
 String n = Console.ReadLine();
 const int Start = 0, End = 100;                          //represent start position i.e 0 & end position i.e 100
-int position = Start;                                      //current position of the player
+int position = Start;                                   //current position of the player
+int counter = 0;         //for no. of times the dice is rolled
 Console.WriteLine(n+" is at the " + position + " position in "+" the starting");
 
 while (position < 100)                                 
 {
-    int dice = new Random().Next(1, 7);                         //gets value dice value
+    int dice = new Random().Next(1, 7);           //gets value dice value
     Console.WriteLine(n + " has got " + dice);
+    counter++;
 
     Console.Write("Lets see ");
     int opt = new Random().Next(0, 3);                            //three values
@@ -20,7 +22,7 @@ while (position < 100)
     {
        Console.Write("Earned a Ladder ......jumped to ");
         position = position + dice;
-        if (position > End)                               //Restart condition
+        if (position > End)                               //End condition
         {
             position = position - dice;
         }
@@ -35,5 +37,7 @@ while (position < 100)
         }
     }
     Console.WriteLine(position);                        //prints the position at the end of one dice throwing process
+    Console.WriteLine();
 }
 Console.WriteLine("Won the game");
+Console.WriteLine("No. of times the dice was rolled = "+counter);       
